336a0bc4409a7e9b63feb9bdb5bf62fd835ea3e0
Samuel Rozario <94673397+Samuel-Rozario@users.noreply.github.com>
2025-09-02 14:59:25 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="phtnai_package",
     version="1.1.0",
-    packages=find_packages('src'),
+    packages=find_packages(where='src'),
     package_dir={'': 'src'},
 )
